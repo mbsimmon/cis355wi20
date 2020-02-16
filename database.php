758b78f1ab43db789c062e90957ba46c55c1c8a3
mbsimmon <42813564+mbsimmon@users.noreply.github.com>
2020-02-15 22:05:09 -0500

    Add files via upload

---
+++ database.php
@@ -0,0 +1,37 @@
+<?php
+class Database
+{
+    private static $dbName = 'mbsimmon355wi20' ;
+    private static $dbHost = '10.8.30.49' ;
+    private static $dbUsername = 'mbsimmon355wi20';
+    private static $dbUserPassword = 'cis355wi202';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+?>
